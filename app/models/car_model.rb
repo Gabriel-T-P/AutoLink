3febba7fa65b811f822874e9c5ea346277831e4f
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-10 19:15:30 -0300

    added enums to model car_model

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,2 +1,5 @@
 class CarModel < ApplicationRecord
+
+  enum :fuel_type, {gasoline: 1, diesel: 3, electric: 5, ethanol: 7, flex: 9}
+  enum :transmission, {manual: 1, automatic: 3, continuously_variable: 5}
 end
