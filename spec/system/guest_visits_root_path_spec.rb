f4bc5d81d19a5c8b44049dcb8a0e2e95ab6ecbfb
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-10 21:43:31 -0300

    Created navbar for app with logo, link for cars index and language dropdown button

---
+++ spec/system/guest_visits_root_path_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'guest visits root path', type: :system do
+  it 'and view navbar' do
+    visit root_path
+
+    within 'nav' do
+      expect(page).to have_link 'Cars', href: cars_path
+      expect(page).to have_link 'English'
+      expect(page).to have_link 'Português'
+    end
+  end
+
+  it 'and changes the language' do
+    visit root_path
+    within 'nav' do
+      click_on 'Português'
+    end
+
+    expect(page).to have_content 'Carros'
+    expect(page).not_to have_content 'Cars'
+  end
+end
