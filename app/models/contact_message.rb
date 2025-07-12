c516d4e1b355237286f279f628b9862ec337d125
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-12 09:40:17 -0300

    added validations to contact_message model and form errors test

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,3 +1,8 @@
 class ContactMessage < ApplicationRecord
   belongs_to :seller
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :phone_number, presence: true, format: { with: /\A\(?\d{2}\)?\s?\d{4,5}-?\d{4}\z/, message: :invalid_phone_format }
+  validates :message, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :seller_id, presence: true
 end
--- spec/system/contact/guest_contatc_seller_spec.rb
+++ spec/system/contact/guest_contatc_seller_spec.rb
@@ -30,7 +30,33 @@ describe 'Guest contact seller', type: :system do
     expect(page).to have_content 'Message sent with success'
   end
 
-  it 'and view errors messages in the form' do
+  it 'and clicks on cancel' do
+    seller = create(:seller, name: 'AutoLink - GR', phone_number: '+55 (99) 999999999')
     
+    visit new_contact_message_path
+    fill_in 'Name', with: 'Name Test'
+    fill_in 'Phone', with: '11911111111'
+    fill_in 'Message', with: 'Really nice car'
+    select 'AutoLink - GR', from: 'Sellers'
+    click_on 'Cancel'
+
+    expect(current_path).to eq root_path
+    expect(page).not_to have_content 'Message sent with success'
+  end
+
+  it 'and view errors messages in the form' do
+    seller = create(:seller, name: 'AutoLink - GR', phone_number: '+55 (99) 999999999')
+    
+    visit new_contact_message_path
+    fill_in 'Name', with: 'a'
+    fill_in 'Phone', with: 'a'
+    fill_in 'Message', with: 'a'
+    click_on 'Send'
+
+    expect(page).to have_content 'Failed to send the message!'
+    expect(page).to have_content 'Please, fix the following problems:'
+    expect(page).to have_content 'Name is too short (minimum is 2 characters)'
+    expect(page).to have_content 'Phone must be in the format (XX) XXXXX-XXXX or (XX) XXXX-XXXX'
+    expect(page).to have_content 'Message is too short (minimum is 10 characters)'
   end
 end
