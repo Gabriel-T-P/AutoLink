296583f3b01dd59c4127289590535454cc3a2c0f
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-07-13 15:40:29 -0300

    fixed bug on turbo frame link

--- spec/system/cars/guest_view_details_of_car_spec.rb
+++ spec/system/cars/guest_view_details_of_car_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Guest view detailed information of car', type: :system do
-  it 'by index page carrousel' do
+  it 'by index page slider' do
     car_model_1 = create(:car_model, brand: 'Hyundai', name: 'Veloster', engine: '1.2 VT', fuel_type: :flex, transmission: :automatic, year: 13.years.ago)
     car_1 = create(:car, price: 80_000, mileage: 23_600, fabricated_at: 11.years.ago, color: 'White', car_model: car_model_1)
     car_model_2 = create(:car_model, brand: 'Crevrolet', name: 'Onix', engine: '1.0', fuel_type: :flex, transmission: :manual, year: 5.years.ago)
@@ -13,7 +13,19 @@ describe 'Guest view detailed information of car', type: :system do
     expect(page).to have_link href: car_path(car_2.code)
   end
 
-  it 'successfully' do
+  it 'by search page' do
+    car_model_1 = create(:car_model, brand: 'Hyundai', name: 'Veloster', engine: '1.2 VT', fuel_type: :flex, transmission: :automatic, year: 13.years.ago)
+    car_1 = create(:car, price: 80_000, mileage: 23_600, fabricated_at: 11.years.ago, color: 'White', car_model: car_model_1)
+    car_model_2 = create(:car_model, brand: 'Crevrolet', name: 'Onix', engine: '1.0', fuel_type: :flex, transmission: :manual, year: 5.years.ago)
+    car_2 = create(:car, price: 50_000, mileage: 75_447, fabricated_at: 4.years.ago, color: 'Black', car_model: car_model_2)
+
+    visit search_cars_path
+
+    expect(page).to have_link href: car_path(car_1.code)
+    expect(page).to have_link href: car_path(car_2.code)
+  end
+
+  it 'successfully by index' do
     seller = create(:seller, name: 'AutoLink - 2')
     car_model = create(:car_model, brand: 'Hyundai', name: 'Veloster', engine: '1.2 VT', transmission: :automatic,
                           car_category: 'Sedan', fuel_type: :gasoline, year: 13.years.ago)
